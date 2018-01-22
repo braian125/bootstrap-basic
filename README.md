db4d18102faae64e95e9ce454b77fd3d080ac030
Brian Vanegas Parra <bvanegas@fga.com.co>
2018-01-22 14:04:54 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Basic Bootstrap
+[GoSoft|Labs]
+
+# Basic version of bootstrap 4.
+
+  - Grid
+  - Layout Utilities
+  - Modal
+
+# Requisites
+
+  - Gulp
+  - Sass compiler
+
+### Note
+
+This project is a fork from https://github.com/braian125/js-taskrunner in addition to the compilation of sass files we have minification of javascript files and image optimization. please read the documentation in js-taskrunner and / or enter gulpfile.js
+
+### How to use
+
+In your root project
+
+  - npm link gulp
+  - npm install
+  - gulp scss
+
+
+### Author
+Brian Vanegas Parra
+@braian125
---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var bust = require('gulp-buster');
+
+gulp.task('sass', function() {
+    return sass('resources/assets/sass/**/*.sass', {style: 'compressed'})
+      .pipe(concat('main.css'))
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('public/css/'))
+      .pipe(bust())
+      .pipe(gulp.dest('public/'));
+});
+
+gulp.task('scss', function() {
+    return sass('resources/assets/scss/**/*.scss', {style: 'compressed'})
+      .pipe(concat('main.css'))
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('public/css/'))
+      .pipe(bust())
+      .pipe(gulp.dest('public/'));
+});
+
+gulp.task('build-js', function() {
+    return gulp.src('resources/assets/js/*.js')    
+      .pipe(concat('main.js'))
+      .pipe(rename({
+        suffix: '.min'        
+      }))
+      .pipe(uglify())
+      .pipe(gulp.dest('public/js'))
+      .pipe(bust())
+      .pipe(gulp.dest('public/'));
+});
+
+gulp.task('images', function() {
+  return gulp.src('resources/assets/images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('public/images'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('resources/assets/js/**/*.js', ['build-js']);
+  gulp.watch('resources/assets/sass/**/*.sass', ['sass']);
+  gulp.watch('resources/assets/scss/**/*.scss', ['scss']);
+  gulp.watch('resources/assets/images/**/*', ['images']);
+});
+
+gulp.task('default', ['build-js','sass','scss','images', 'watch']);
